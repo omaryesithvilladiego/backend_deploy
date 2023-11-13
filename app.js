d044f585d2f405c226d9b6b2b82d3736525b98ca
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2023-11-13 16:20:36 -0500

    Cambios para solucionar error imagenes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
-
+app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,11 +33,13 @@ app.use('/public', express.static(`${__dirname}/storage`))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
+
+
 app.use('/usuarios', usuarioRoute)
 app.use('/estudiante', estudianteRoute)
 
 app.use(function(req, res, next) {
-   res.send('From Index')
+   res.send('from index' )
 });
 
 //Database conection settings
