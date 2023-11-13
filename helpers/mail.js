e968d37c4077d9c338e284bf3a25dac3d028afe4
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2023-11-13 13:03:11 -0500

    last commit update

--- helpers/mail.js
+++ helpers/mail.js
@@ -1,13 +1,32 @@
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
+
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.office365.com",
+//     port: 587,
+//     secure: false, // upgrade later with STARTTLS
+//     auth: {
+//       user: "omar.villadiegoc@campusucc.edu.co",
+//       pass: "_+_+gTA;J4Yyh6K",
+//     },
+//   });
+
+//   module.exports = transporter
+
+
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false, // upgrade later with STARTTLS
-    auth: {
-      user: "omar.villadiegoc@campusucc.edu.co",
-      pass: "_+_+gTA;J4Yyh6K",
-    },
-  });
+  service: 'gmail',
+  auth: {
+    user: 'villadiegoomar476@gmail.com', // Tu dirección de correo de Gmail
+    pass: 'ekvm jfns ecot dkmm', // La contraseña de aplicación generada
+  },
+  secure: true,
+  port: 465,
+});
 
-  module.exports = transporter
+module.exports = transporter;
+
+
+
+  // ekvm jfns ecot dkmm
