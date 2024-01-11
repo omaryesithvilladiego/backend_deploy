60c4145a232177d5fddc0b63ba7fd407be9b9e10
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2024-01-11 00:07:54 -0500

    last coomit backend 0001

--- app.js
+++ app.js
@@ -65,10 +65,6 @@ app.use('/ponencia' , auth, ponenciaRouter)
 app.use('/proyecto',auth,proyectoRoute)
 
 
-app.use("/", (req,res) => {
-  res.send("From Index")
-})
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
